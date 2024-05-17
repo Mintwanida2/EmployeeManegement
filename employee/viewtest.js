7b7c19fa5dcb6dda0f07a9ed7b48e56e8e86a34b
Mintwanida2 <wanidamint88@gmail.com>
2024-05-17 17:45:16 +0700

    fix header

--- employee/viewtest.js
+++ employee/viewtest.js
@@ -5,7 +5,7 @@ function render(emps) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie Data</title>
+    <title>Employee Data</title>
     <link rel="stylesheet" href="/public/output.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
